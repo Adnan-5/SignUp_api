a07313c7915803e8f045892b13663345224f7069
Adnan-5 <sahilladnan810@gmail.com>
2022-06-29 19:25:15 +0500

    validate the user inputs

--- routes/signUp.js
+++ routes/signUp.js
@@ -6,6 +6,9 @@ require("dotenv").config()
 router.post("/", async (req, res ,next) =>{
       try {
         const { first_name, last_name,email, password} = req.body
+      if(!(email && password && first_name && last_name)){
+        res.status(400).send("All Fields ")
+      }
       } catch (error) {
         
       }
