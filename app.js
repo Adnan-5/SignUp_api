c7032cbce239716efb3a64bbe1e8464897174a1e
Adnan-5 <sahilladnan810@gmail.com>
2022-06-29 16:57:32 +0500

    routes handling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require("express")
 let app = express();
 const routes = require("./routes/signUp")
+app.use("/signUp", routes)
 let port = process.env.PORT || 3000;
 
 app.listen(port, () =>{
