7ba4ee6f3283c3fae6b5483381e2739e1d340b57
Adnan-5 <sahilladnan810@gmail.com>
2022-06-29 16:30:07 +0500

    basic signUp route created

--- app.js
+++ app.js
@@ -3,5 +3,11 @@ let app = express();
 
 let port = process.env.PORT || 3000;
 
-
+app.get("/signUp", (req, res, next) => {
+    res.json({
+        body:{
+            message:"hello its Home Route"
+        }
+    })
+})
 
