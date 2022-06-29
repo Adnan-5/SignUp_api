63daa8aea039819409b7dac52e25d9be3f640e9c
Adnan-5 <sahilladnan810@gmail.com>
2022-06-29 16:55:28 +0500

    basic testing route removed

--- app.js
+++ app.js
@@ -3,13 +3,6 @@ let app = express();
 
 let port = process.env.PORT || 3000;
 
-app.get("/signUp", (req, res, next) => {
-    res.json({
-        body:{
-            message:"hello its Home Route"
-        }
-    })
-})
 app.listen(port, () =>{
     console.log('server is listning on port', port);
 })
