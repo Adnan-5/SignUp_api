2000494285d8a0d7e4dea34e816dda2e549ed3c3
Adnan-5 <sahilladnan810@gmail.com>
2022-06-29 19:58:52 +0500

    response updated if input not completely filled

--- routes/signUp.js
+++ routes/signUp.js
@@ -2,12 +2,11 @@ const regUser = require("../models/User")
 const express = require("express");
 const router = express.Router()
 require("dotenv").config()
-
 router.post("/", async (req, res ,next) =>{
       try {
         const { first_name, last_name,email, password} = req.body
       if(!(email && password && first_name && last_name)){
-        res.status(400).send("All Fields ")
+        res.status(400).send("All Fields  Are Required")
       }
       const user = await User.create({
         first_name,
