79e40f1651607ca5a939f4bd61af2f4ff9e8f833
Adnan-5 <sahilladnan810@gmail.com>
2022-06-29 20:06:10 +0500

    bug fix

--- routes/signUp.js
+++ routes/signUp.js
@@ -11,7 +11,7 @@ router.post("/", async (req, res ,next) =>{
       if(!(email && password && first_name && last_name)){
         res.status(400).send("All Fields  Are Required")
       }
-      const user = await User.create({
+      const user = await regUser.create({
         first_name,
         last_name,
         email: email.toLowerCase(), // sanitize: convert email to lowercase
