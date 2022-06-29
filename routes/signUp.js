407354e818ad1d4c1fdb5dcd774df6bfb268c923
Adnan-5 <sahilladnan810@gmail.com>
2022-06-29 19:55:03 +0500

    create user

--- routes/signUp.js
+++ routes/signUp.js
@@ -9,6 +9,13 @@ router.post("/", async (req, res ,next) =>{
       if(!(email && password && first_name && last_name)){
         res.status(400).send("All Fields ")
       }
+      const user = await User.create({
+        first_name,
+        last_name,
+        email: email.toLowerCase(), // sanitize: convert email to lowercase
+        password: password,
+      });
+
       } catch (err) {
         return err.message
       }
